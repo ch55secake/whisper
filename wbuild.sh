69ab383cb24d08a9d45747715ce4b5d3935b5cc4
ch55secake <oscardjbackup@gmail.com>
2024-12-30 02:21:37 +0000

    chore: add loop to bash script

--- wbuild.sh
+++ wbuild.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 # Watch changes, call the other script to kill and rebuild the client 
-fswatch -o ~/Projects/whisper/client/ | xargs -n1 -I{} ./kbuild.sh
+while true; do 
+	fswatch -o ~/Projects/whisper/client/ | xargs -n1 -I{} ./kbuild.sh && echo "Change detected... rebuilding"
+done
